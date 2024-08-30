f492686ecbec6d1bd7bf9ba903df1e5516e4d7b6
Haniyeh Z. Budaqi <87523819+hannalujah@users.noreply.github.com>
2024-08-30 13:46:41 +0330

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,16 +4,16 @@ disturbance = [1,1,1,2,2,2,3,3,3,4,4,4]
 random.shuffle(disturbance)
 
 arduino = serial.Serial(port='COM5', baudrate=115200, timeout=0.1)
-
-for i in range(len(disturbance)):
-    dist_type = str(disturbance[i])
-    arduino.write(bytes(dist_type,  'utf-8'))
-    time.sleep(0.5)
-    data = arduino.readline().decode('utf-8').strip('\r\n')
-    if data == '':
-        print('0')
-    else:
-        print(data)
+if arduino.is_open:
+    for i in range(len(disturbance)):
+        dist_type = str(disturbance[i])
+        arduino.write(bytes(dist_type,  'utf-8'))
+        time.sleep(0.5)
+        data = arduino.readline().decode('utf-8').strip('\r\n')
+        if data == '':
+            print('0')
+        else:
+            print(data)
 
 
 arduino.close()
